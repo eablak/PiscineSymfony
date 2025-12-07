42f08d84e169d93cdc0f33185b48452e442ef900
eablak <esrablk9@gmail.com>
2025-12-07 10:44:43 +0300

    user login ajax

---
+++ Symfony-3-Final/src/Controller/UserController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+final class UserController extends AbstractController
+{
+    #[Route('/login', name: 'app_login', methods: ['GET', 'POST'])]
+    public function loginAction(Request $request, AuthenticationUtils $authenticationUtils)
+    {
+
+        if($request->request->get('some_var_name') && $this->getUser()){
+            $arrData = ['output' => 'Successfully logged in!'];
+            return new JsonResponse($arrData);
+        }
+
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $last_username = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', array('error' => $error, 'last_username' => $last_username));
+    }
+
+
+    #[Route('/login_check', name: 'app_login_check', methods: ['POST'])]
+    public function loginCheck(){ 
+    }
+}
