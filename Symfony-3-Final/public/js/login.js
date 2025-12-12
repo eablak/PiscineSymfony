3d4c5a024259b6617f06b5f149190cfc6cc994ea
eablak <esrablk9@gmail.com>
2025-12-12 11:26:45 +0300

    usercontroller

--- Symfony-3-Final/public/js/login.js
+++ Symfony-3-Final/public/js/login.js
@@ -4,18 +4,11 @@ $(document).on('click', '.ajax-login', function(e){
 
     $.post('/ajax/login', $('#login-form').serialize(), function(response){
 
-        if (response.success){
-            $('#loginForm-container').hide();
-            $.get('/ajax/postForm', function(formHtml){
-                $('#postForm-container').html(formHtml).show();
-            });
-            $.get('/ajax/postList', function(tableHtml){
-                $('#postList-container').html(tableHtml).show();
-            });
-        }else{
-            alert('Invalid Credentials!')
+        if (response.error){
+            $('#ajax-results').html(response.error).show();
+            return;
         }
-
+        $('#ajax-results').html(response.success).show();
     });
 
 
--- Symfony-3-Final/src/Controller/UserController.php
+++ Symfony-3-Final/src/Controller/UserController.php
@@ -8,6 +8,11 @@ use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use App\Repository\UserRepository;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+
+
 
 final class UserController extends AbstractController
 {
@@ -15,20 +20,33 @@ final class UserController extends AbstractController
     public function loginAction(Request $request, AuthenticationUtils $authenticationUtils)
     {
 
-        if($request->request->get('login_ajax') && $this->getUser()){
-            $arrData = ['output' => 'Successfully logged in!'];
-            return new JsonResponse($arrData);
-        }
-
         $error = $authenticationUtils->getLastAuthenticationError();
         $last_username = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', array('error' => $error, 'last_username' => $last_username));
+        return $this->render('login/login.html.twig', array('error' => $error, 'last_username' => $last_username));
     }
 
 
-    #[Route('/login_check', name: 'app_login_check', methods: ['POST'])]
-    public function loginCheck(){
-   
+    #[Route('/ajax/login', name: 'ajaxLogin', methods: ['POST'])]
+    public function loginCheck(Request $request, UserRepository $userRepository, TokenStorageInterface $tokenStorage){ 
+
+        $username = $request->request->get('username');
+        $password = $request->request->get('password');
+
+        $user = $userRepository->findOneBy(['username' => $username]);
+
+        if (!$user) {
+            return new JsonResponse(['error' => 'Invalid Credentials!']);
+        }
+
+        if (!password_verify($password, $user->getPassword())) {
+            return new JsonResponse(['error' => 'Invalid Credentials!']);
+        }
+
+        $token = new UsernamePasswordToken($user, 'main', $user->getRoles());
+        $tokenStorage->setToken($token);
+        $request->getSession()->set('_security_main', serialize($token));
+
+        return new JsonResponse(['success' => 'Succesfully Login!']);
     }
 }
